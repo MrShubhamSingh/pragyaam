949bd48c5f4cabdc58a3168ae76cbc6364c40df9
MrShubhamSingh <shubhamitd237@gmail.com>
2018-12-20 01:17:23 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Result from './Result';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Screen from './screen2';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div>
+      <Route exact path="/" component={Result} />
+      <Route exact path="/screen2/:id" component={Screen} />
+      </div>
+      </BrowserRouter>
       </div>
     );
   }
